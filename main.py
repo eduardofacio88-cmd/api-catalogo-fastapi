bf2537d2dbe46a58c38a069a75848a421307b4b3
eduardofacio88-cmd <eduardofacio88@gmail.com>
2026-05-06 11:36:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from repl-nix-workspace!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
+app = FastAPI(title="API de Catálogo de Mercado 🛒")
 
-if __name__ == "__main__":
-    main()
+# 1. Definimos o modelo do produto (o esqueleto)
+class Produto(BaseModel):
+    nome: str
+    preco: float
+    categoria: str
+    marca: str
+
+# Banco de dados temporário em memória (uma lista Python)
+banco_produtos: List[Produto] = []
+
+# 2. Rota para cadastrar um novo produto (POST)
+@app.post("/produtos", response_model=Produto)
+def cadastrar_produto(produto: Produto):
+    banco_produtos.append(produto)
+    return produto
+
+# 3. Rota para listar todos os produtos cadastrados (GET)
+@app.get("/produtos", response_model=List[Produto])
+def listar_produtos():
+    return banco_produtos
